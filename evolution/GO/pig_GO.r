fe550376e70ee7d635a8b4dfeef2c0072f8ba67a
binzheng <87288334+binzhengbin@users.noreply.github.com>
2025-03-28 00:03:15 +0800

    Create pig_GO.r

---
+++ evolution/GO/pig_GO.r
@@ -0,0 +1,42 @@
+setwd('E:/02-群体进化/多物种/GO/202503/pig/selection/top1/3method_GO/')
+library(dplyr)
+library(clusterProfiler)
+library(DOSE)
+library(GO.db)
+require(AnnotationHub)
+library(org.Ss.eg.db)
+options(stringsAsFactors = FALSE) 
+gene_files <- list.files(path = '../3method_gene/', pattern = "*gene.txt", full.names = TRUE)
+
+# 循环处理每个基因文件
+for (file in gene_files) {
+  # 读取基因文件
+  gene <- read.table(file, header = TRUE, sep = "\t")
+  
+  # 提取基因列表
+  unique_genes <- unique(gene$x)
+  symbol <- as.character(unique_genes)
+  
+  # 转换基因符号为 ENTREZID
+  eg <- bitr(symbol, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = org.Ss.eg.db)
+  id <- as.character(eg[, 2])
+  
+  # 进行 GO 富集分析
+  ego <- enrichGO(gene = id,
+                  OrgDb = "org.Ss.eg.db",
+                  ont = "all",
+                  pAdjustMethod = "BH",
+                  pvalueCutoff = 1,
+                  qvalueCutoff = 1,
+                  readable = TRUE)
+  
+  # 过滤 p 值小于 0.05 的结果
+  ego <- as.data.frame(ego)
+  ego <- ego[ego$pvalue < 0.05, ]
+  
+  # 生成输出文件名
+  output_file <- sub("gene.txt", "GO.txt", basename(file))
+  
+  # 保存结果
+  write.table(ego, file = output_file, sep = "\t", row.names = FALSE)
+}
