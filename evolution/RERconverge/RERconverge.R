e53e69712b2706779a2e03ca3ca6a2f671549e88
Bin Zheng <87288334+BinZheng1998@users.noreply.github.com>
2025-07-08 22:15:24 +0800

    Update RERconverge.R

--- evolution/RERconverge/RERconverge.R
+++ evolution/RERconverge/RERconverge.R
@@ -12,7 +12,8 @@ library(ape)
 #raw_seqs <- read.FASTA("test/NP_005036.2_trimmed.fa", type = "AA")
 #lengths <- nchar(raw_seqs)
 #print(table(lengths))
-estimatePhangornTreeAll(alndir = 'RERconverge_input/',treefile = 'ucsc100way_species_tree.nwk',output.file = 'RERconverge_output/geneTree')
+
+#estimatePhangornTreeAll(alndir = 'RERconverge_input/', treefile = 'ucsc100way_species_tree.nwk',output.file = 'RERconverge_output/geneTree') #此代码运行一次以后生成geneTree就可以不再运行，并且这个步骤特别耗费时间。
 tree <- readTrees('RERconverge_output/geneTree')
 res <- getAllResiduals(tree,transform = 'sqrt',weighted = T,scale = T)
 #这里的树其它物种分支长度为0，目标物种为1，需要对tree进行修改。但是拓扑结果不能变。ucsc100way_species_tree_noCGM.nwk
@@ -20,14 +21,13 @@ marineb <- read.tree('ucsc100way_species_tree_noCGM.nwk')
 
 phenvMarine=tree2Paths(marineb, tree)
 cor_res=correlateWithBinaryPhenotype(res, phenvMarine, min.sp=10, min.pos=4,weighted="auto")
-cor_res
 write.table(cor_res,file = 'RERconverge_res.txt',sep = '\t')
 
 #置换检验
 foreground = c("bosTau8","susScr3","canFam3","galGal4","oviAri3")
 root_sp<-"latCha1"
 masterTree <- tree$masterTree
-perms = getPermsBinary(numperms=10, 
+perms = getPermsBinary(numperms=10000, 
                        fg_vec=foreground, 
                        sisters_list=NULL, 
                        root_sp=root_sp, 
@@ -73,26 +73,52 @@ RER_res <- RER_res[,-9]
 write.table(RER_res,file = 'RERconverge_res_20250708.txt',sep = '\t')
 
 #GO
-rer_gene <- RER_res[RER_res$P<0.05,]
-rer_gene<-na.omit(rer_gene)
 library(clusterProfiler)
 library(org.Gg.eg.db)
 library(DOSE)
 library(GO.db)
 library(aPEAR)
-chicken_genes<- rer_gene$chicken
-eg <- bitr(chicken_genes, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Gg.eg.db")
+rer_gene <- RER_res[RER_res$P<0.05,]
+rer_gene<-na.omit(rer_gene)
+#Rho > 0
+chicken_genes1 <- rer_gene[rer_gene$Rho>0,]
+chicken_genes1 <- chicken_genes1$chicken
+eg <- bitr(chicken_genes1, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Gg.eg.db")
 id <- as.character(eg[, 2])
-ego <- enrichGO(gene = id, OrgDb = "org.Gg.eg.db", ont = "all",
+ego1 <- enrichGO(gene = id, OrgDb = "org.Gg.eg.db", ont = "all",
                 pAdjustMethod = "BH", pvalueCutoff = 1, qvalueCutoff = 1,
                 readable = TRUE)
-ego <- as.data.frame(ego)
-ego <- ego[ego$pvalue < 0.05,]
+ego1 <- as.data.frame(ego1)
+ego1 <- ego1[ego1$pvalue < 0.05,]
+write.table(ego1,file = 'RERconverge_acceleration_chickenGO.txt',sep = '\t')
 
-p<-enrichmentNetwork(ego,colorBy = 'pvalue',
+p1<-enrichmentNetwork(ego1,colorBy = 'pvalue',
                      colorType = 'pval',
                      nodeSize = 'zScore',
                      fontSize = 2,
                      drawEllipses = T,
                      verbose = F)
-p
+p1
+ggsave('RERconverge_acceleration_chickenGO.pdf',p1,width = 10,height = 6,dpi=500)
+
+#Rho < 0
+chicken_genes2 <- rer_gene[rer_gene$Rho<0,]
+chicken_genes2 <- chicken_genes2$chicken
+eg <- bitr(chicken_genes2, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Gg.eg.db")
+id <- as.character(eg[, 2])
+ego2 <- enrichGO(gene = id, OrgDb = "org.Gg.eg.db", ont = "all",
+                 pAdjustMethod = "BH", pvalueCutoff = 1, qvalueCutoff = 1,
+                 readable = TRUE)
+ego2 <- as.data.frame(ego2)
+ego2 <- ego2[ego2$pvalue < 0.05,]
+write.table(ego2,file = 'RERconverge_deceleration_chickenGO.txt',sep = '\t')
+
+p2<-enrichmentNetwork(ego2,colorBy = 'pvalue',
+                      colorType = 'pval',
+                      nodeSize = 'zScore',
+                      fontSize = 2,
+                      drawEllipses = T,
+                      verbose = F)
+p2
+ggsave('RERconverge_deceleration_chickenGO.pdf',p1,width = 10,height = 6,dpi=500)
+
