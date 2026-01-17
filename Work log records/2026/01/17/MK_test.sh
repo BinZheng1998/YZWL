7491fd43ca605bb8aa10b759b20d254053db878a
Bin Zheng <87288334+BinZheng1998@users.noreply.github.com>
2026-01-17 21:04:02 +0800

    Create MK_test.sh

---
+++ Work log records/2026/01/17/MK_test.sh
@@ -0,0 +1,6 @@
+degenotate.py 
+  -g ~/ref/huxu_v23_ref/chicken.ref_noMT.fa 
+  -a ~/ref/huxu_v23_ref/20250514_final_ref/20250529_chicken_no_RUM_chrMT.gtf 
+  -v ~/Pop/dna/snp/05_clean_vcf_phased/BW_2036samples/merged_beagle.vcf.gz 
+  -u ~/project/01_evolution/convergent_evolution/01_chicken/metadata/body_weight_low_no_wildjungleflow_noyunnan.txt 
+  -o res
