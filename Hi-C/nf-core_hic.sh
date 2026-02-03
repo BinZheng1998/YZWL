69956e35d2b733ee56a13271ffaf5e9032787a53
Bin Zheng <87288334+BinZheng1998@users.noreply.github.com>
2026-02-03 09:51:12 +0800

    Update nf-core_hic.sh

--- Hi-C/nf-core_hic.sh
+++ Hi-C/nf-core_hic.sh
@@ -11,3 +11,18 @@ nextflow run nf-core/hic
 -c ~/HIC/script/custom.config \
 --res_tads '10000,20000,40000' \
 -resume
+
+#
+ nextflow run nf-core/hic \
+   -profile singularity \
+   --input ./sample.csv \
+   --fasta ~/HIC/ref/chicken/chicken.ref.fa \
+   --digestion 'dpnii' \
+   --bwt2_index ~/HIC/ref/chicken \
+   --split_fastq \
+   --fastq_chunks_size '10000000' \
+   --bin_size '20000,40000,100000,250000,1000000' \
+   --tads_caller 'insulation,hicexplorer' \
+   -c ~/HIC/script/custom2.config \
+   --res_tads '10000,20000,40000' \
+   -resume
