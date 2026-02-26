2feb23826f223ba63686fb5839d804db7a87ec8e
Bin Zheng <87288334+BinZheng1998@users.noreply.github.com>
2026-02-26 21:47:28 +0800

    Update chromap.sh

--- Hi-C/chromap.sh
+++ Hi-C/chromap.sh
@@ -8,3 +8,9 @@
   -1 SRR28164016_1.clean.fastq.gz \
   -2 SRR28164016_2.clean.fastq.gz \
   -o SRR28164016.chromap.pairs
+
+#2
+pairtools sort CRR1137296.chromap.pairs --nproc 22 --memory 128G --nproc-in 22 --nproc-out 22 -o CRR1137296.chromap_sorted.pairs
+
+#3
+pairtools dedup CRR1137296.chromap_sorted.pairs --max-mismatch 1 --method max --nproc-in 22 --nproc-out 22 -o CRR1137296.chromap_sorted_nodups.pairs
