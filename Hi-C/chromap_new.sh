6f1f1bf37cf375bd7d9760b13f08a499146c0d41
Bin Zheng <87288334+BinZheng1998@users.noreply.github.com>
2026-02-27 11:03:21 +0800

    Update chromap_new.sh

--- Hi-C/chromap_new.sh
+++ Hi-C/chromap_new.sh
@@ -1,22 +1,33 @@
-# 1. 直接用 chromap 完成比对、排序、去重 (一步到位)
+# 1. Chromap 一步到位：比对 + 过滤 + 去重 + 排序 + 压缩
+# 使用 --remove-pcr-duplicates 替代 pairtools dedup
+# 直接输出 .gz 后缀，chromap 会自动进行 bgzip 压缩
 ~/software/chromap/chromap \
   --preset hic \
-  --remove-duplicates \
+  --remove-pcr-duplicates \
+  --low-mem \
   -t 24 \
   -x ../chromap_index/chicken_ref \
   -r ../chromap_index/chicken.ref.fa \
   -1 SRR28164016_1.clean.fastq.gz \
   -2 SRR28164016_2.clean.fastq.gz \
-  -o SRR28164016.pairs.gz  # 直接输出 gz
+  --pairs \
+  -o SRR28164016.pairs.gz
 
-# 2. 建立索引 (pairix 索引是很多下游工具的必需品)
+# 2. 建立索引 (pairix)
+# 虽然 chromap 输出了 pairs，但 cooler cload pairix 模式需要这个索引来提速
 ~/software/pairix/bin/pairix SRR28164016.pairs.gz
 
-# 3. 生成高分辨率 cool 文件 (建议 5000，即 5kb)
-cooler cload pairix ../chicken.chrom.sizes:5000 \
+# 3. 生成高分辨率 Cooler 文件 (以 5kb 为起始)
+# 只有起始分辨率够高，后面的 zoomify 才有意义
+cooler cload pairix \
+  ../chicken.chrom.sizes:5000 \
   SRR28164016.pairs.gz \
   SRR28164016_5k.cool
 
-# 4. 生成多分辨率 mcool 并自动归一化
-# 这里如果不指定 --resolutions，它会默认从 5k 生成到几 Mb
-cooler zoomify -n 25 --balance SRR28164016_5k.cool -o SRR28164016.mcool
+# 4. 生成多分辨率矩阵并自动归一化 (Balance)
+# 这会生成一个包含 5k, 10k, 25k, 50k, 100k... 等所有分辨率的 mcool 文件
+cooler zoomify \
+  -n 24 \
+  --balance \
+  SRR28164016_5k.cool \
+  -o SRR28164016.mcool
