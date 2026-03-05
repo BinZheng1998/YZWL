d1d0bd53cf3e4592364d61476226ff76844a64c7
Bin Zheng <87288334+BinZheng1998@users.noreply.github.com>
2026-03-05 09:53:12 +0800

    Update isoseq.sh

--- Work log records/2026/03/04/isoseq.sh
+++ Work log records/2026/03/04/isoseq.sh
@@ -2,3 +2,4 @@ cat *fastq.gz > data.fastq.gz
 seqtk seq -A data.fastq.gz > data.fasta 
 ~/software/pbmm2 index ~/ref/huxu_v23_ref/chicken.ref.fa chicken.mmi
 ~/software/pbmm2 align ~/ref/huxu_v23_ref/chicken.ref.fa data.fasta res.bam --preset ISOSEQ --sort -j 24 --log-level INFO 2>pbmm2.log &
+isoseq3 collapse res.bam collapsed.gff
