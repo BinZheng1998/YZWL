da025fae0109e34b5d63715e2d2e36252e12345d
Bin Zheng <87288334+BinZheng1998@users.noreply.github.com>
2026-03-06 20:30:13 +0800

    Update fastPHASE.sh

--- Work log records/2026/03/05/fastPHASE.sh
+++ Work log records/2026/03/05/fastPHASE.sh
@@ -1,3 +1,4 @@
+参考教程：https://htmlpreview.github.io/?https://github.com/inzilico/imputeqc/blob/master/vignettes/k_selection.html
 plink --bfile cattle_BW --chr chr1 --recode fastphase-1chr --out cattle_BW_chr1 --chr-set 31
 # bed bim fam文件
 Rscript make_test_files.R cattle_BW_chr1.recode.phase.inp
