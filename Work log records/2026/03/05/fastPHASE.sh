c5aa463a3bcc1974e2aa1b5a4c56405796b87577
Bin Zheng <87288334+BinZheng1998@users.noreply.github.com>
2026-03-06 20:29:49 +0800

    Create fastPHASE.sh

---
+++ Work log records/2026/03/05/fastPHASE.sh
@@ -0,0 +1,4 @@
+plink --bfile cattle_BW --chr chr1 --recode fastphase-1chr --out cattle_BW_chr1 --chr-set 31
+# bed bim fam文件
+Rscript make_test_files.R cattle_BW_chr1.recode.phase.inp
+#这一步非常慢 因为snp数量多
